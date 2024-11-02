5176f1ab23894fe76866d29bd569cecf9205c8da
JFLino <67127636+JFLino@users.noreply.github.com>
2024-11-02 14:14:21 +0500

    Update task1.ino

--- task1/task1.ino
+++ task1/task1.ino
@@ -1,32 +1,26 @@
-
 #define BTN 33
-
 #define RED 4
 #define GRN 5
 #define YLW 19
 #define BLU 22
 
 void setup() {
-  // configure button as input
   pinMode(BTN, INPUT);
-  // configure serial port speed
+
   Serial.begin(115200);
 }
 
 void loop() {
-  // this variable keeps the counting number of button pressed event
-  static int count = 0;
-  // this variable keeps its value even the function is called multiple times
+  static int count;
   static int prev_btn = LOW;
-  // read current button state (HIGH means pressed)
-  int btn = digitalRead(BTN);
 
-  // detect button pressed event
-  if (btn == HIGH && prev_btn == LOW){
+  int btn = digitalRead(BTN);
+  //detect button pressed moment
+  if(btn == HIGH && prev_btn == LOW){
     count++;
     Serial.println(count);
   }
-  // save current btn state into a variable to be used in next loop iteration
+
   prev_btn = btn;
-  
+  delay(100);
 }
