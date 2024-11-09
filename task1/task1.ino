17d34c64ae9234bdcd0b5e01ec93b909a8ad0e13
JFLino <67127636+JFLino@users.noreply.github.com>
2024-11-09 14:43:26 +0500

    09.11.2024 lab4

--- task1/task1.ino
+++ task1/task1.ino
@@ -1,8 +1,17 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
 #define BTN 33
 #define RED 4
 #define GRN 5
 #define YLW 19
 #define BLU 22
+
+const char* ssid = "#Turin.uz";
+const char* pass = "Turin_2024@!";
+
+WiFiClient wifi_client;
+PubSubClient mqtt_client(wifi_client);
 
 void setup() {
   pinMode(BTN, INPUT);
@@ -15,9 +24,50 @@ void setup() {
   pinMode(YLW, OUTPUT);
   pinMode(BLU, OUTPUT);
 
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.println("trying to connect....");
+    delay(1000);
+  }
+
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("MAC: ");
+  Serial.println(WiFi.macAddress());
+
+  mqtt_client.setServer("mqtt.iotserver.uz", 1883);
+  mqtt_client.setCallback(my_callback);
+
 }
 
 void loop() {
+  while(!mqtt_client.connected()){
+    //generate unique id
+    String myID = "edacs_lab4";
+    String macAddr = WiFi.macAddress();
+    String macSuffix = macAddr.substring(macAddr.length() - 5);
+    macSuffix.replace(":", "");
+    myID += macSuffix;
+
+    //connect to mqtt broker
+    int mres = mqtt_client.connect(myID.c_str(), "userTTPU", "mqttpass");
+    if (mres){
+      Serial.print("mqtt connected: ");
+      Serial.println(mres);
+      mqtt_client.subscribe("ttpu/edacs/lab4");
+      mqtt_client.publish("ttpu/edacs/msg", "hello fuadov");
+    }else{
+      Serial.print("mqtt NOT connected, trying ... ");
+      Serial.println(mres);
+      delay(3000);
+    }
+  }
+
+  mqtt_client.loop();
+
   static int count;
   static int prev_btn = LOW;
 
@@ -50,4 +100,8 @@ void loop() {
 
   prev_btn = btn;
   delay(100);
+}
+
+void my_callback(char* topic, byte* payload, unsigned int len){
+  Serial.println("Msg received");
 }
